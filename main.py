95c0685f1de0bb7a82451314725f7600fefcf3f4
Tauseed <tauseedzamanofsorana@gmail.com>
2021-06-20 03:18:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import time
 
 # wait for 3 sec
 time.sleep(3)
-# start from 0 and go on ...
+# start from 0 and move ......
 i =0
 
 while True:
 	# wait for 0.2 sec
 	time.sleep(0.2)
-	# write in the fucesed field number and imoji
+	# write in the focused field number and imoji
 	pyautogui.typewrite(""+str(i)+" :)")
 	# press enter to sumbit
 	pyautogui.press('enter')
